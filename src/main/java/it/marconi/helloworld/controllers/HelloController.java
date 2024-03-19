f74e26ac9b62a3109ad4843ae93b6db8aaa26014
StefanoMarinelli <stefano.marinelli@marconirovereto.it>
2024-03-19 09:36:51 +0100

    Inizio applicazione

---
+++ src/main/java/it/marconi/helloworld/controllers/HelloController.java
@@ -0,0 +1,12 @@
+package it.marconi.helloworld.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+//Stereotype annotation
+@Controller
+@RequestMapping("/")
+public class HelloController {
+    
+    //CRUD Create Read Update Delete --> HTTP Post, Get, Put, Delete
+}
