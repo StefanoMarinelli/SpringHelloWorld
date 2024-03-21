b6a68446475fc44a5c01ce7e14e50eb0bca7826b
StefanoMarinelli <stefano.marinelli@marconirovereto.it>
2024-03-21 13:45:49 +0100

    LUPA CHANGE CON KOKSAL

--- src/main/java/it/marconi/helloworld/controllers/HelloController.java
+++ src/main/java/it/marconi/helloworld/controllers/HelloController.java
@@ -1,7 +1,10 @@
 package it.marconi.helloworld.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 //Stereotype annotation
 @Controller
@@ -9,4 +12,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HelloController {
     
     //CRUD Create Read Update Delete --> HTTP Post, Get, Put, Delete
+    @GetMapping
+	public String greet(){
+		return "hello-world";
+	}
+
+    @GetMapping(params="lupa")
+    public String greetParameter() {
+        return "hello-world-param";
+    }
+    
 }
