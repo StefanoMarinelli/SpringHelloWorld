0e58065f9ef139640a62fb2e3ea4396f9c93f9d8
StefanoMarinelli <stefano.marinelli@marconirovereto.it>
2024-04-04 13:28:25 +0200

    Waikiki Committo

--- src/main/java/it/marconi/helloworld/controllers/HelloModelController.java
+++ src/main/java/it/marconi/helloworld/controllers/HelloModelController.java
@@ -2,6 +2,7 @@ package it.marconi.helloworld.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -22,5 +23,11 @@ public class HelloModelController {
     @GetMapping("Bigoluz")
     public ModelAndView greetParameter(@RequestParam(name = "Finduskers", defaultValue = "Barbospoof") String name){
         return new ModelAndView("hello-world-model").addObject("Finduskers", name);
+    }
+
+    //Passa un parametro recuperato dal path URL
+    @GetMapping("/{fuckingUsername}")
+    public ModelAndView greetPlaceholder(@PathVariable("fuckingUsername") String NomeNdoGulo){
+        return new ModelAndView("hello-world-username").addObject("fuckingUsername", NomeNdoGulo);
     }
 }
